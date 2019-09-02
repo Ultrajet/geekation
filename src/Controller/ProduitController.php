6c053283aac5194fa5315e40534f7f3aae189cec
Quentin <quentin.lagadou@gmail.com>
2019-09-02 23:34:25 +0200

    commentaires ProduitController

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -31,28 +31,34 @@ class ProduitController extends AbstractController
      */
     public function produit($slug, Request $request)
     {
+        // ----------------------
+        // FICHE PRODUIT
+        // ----------------------
+
+        // il faut /produit/{slug} dans l'URL : ce {slug} sert à sélectionner le premier examplaire du produit qui apparait dans la table Produits, et afficher ses infos dans la vue
         $produit = $this->getDoctrine()
         ->getRepository(Produits::class)
         ->findOneBy(['slug' => $slug]);
 
+        // par le biais de ce produit sélectionné, on récupère la ou les ProduitsCommandes qui correspondent
+        // pour l'instant $produit (qui est un objet) ne contient QU'UN SEUL exemplaire du produit, avec son propre ID
         $produitCommande = $this->getDoctrine()
         ->getRepository(ProduitsCommandes::class)
         ->findOneBy(['produit' => ($produit->getId())]);
+        // prochaine étape : regrouper dans un array tous les exemplaires du produit pour ensuite avoir tous les ProduitsCommandes liés au produit global
 
+        // mmmmh...
         $produitId = $produit->getId();
 
+        // c'était pour générer les select des dates de location
+        // pourrait servir avec les datepicker... (les objets DateTime sont capricieux à gérer)
         $dateNow = new DateTime();
         $dateArray = [$dateNow->format('d/m')];
         for ($i = 0; $i < 7; $i++) {
             array_push($dateArray, $dateNow->modify('+1 day')->format('d/m'));
-        }
+        };
 
-        // $produitCommande = $produit->a();
-
-        // if ($this->handleRequest($request)) {
-        //     echo "Ouais!";
-        // }
-
+        // on envoie tout ça dans le template
         return $this->render('produit/show.html.twig', [
             'produit' => $produit,
             'produitCommande' => $produitCommande,
@@ -102,23 +108,39 @@ class ProduitController extends AbstractController
      */
     public function ajoutPanier($slug, Request $request)
     {
+        // ----------------------
+        // AJOUT PANIER
+        // ----------------------
+
+        // cette route est nécessaire pour ajouter un produit au panier en ajax
+        // methods={"POST"} sert à empêcher de taper une URL genre /ajoutpanier/lorem-0 où le {slug} arriverait dans $_GET : celui-ci doit obligatoirement arriver dans $_POST, donc à travers le formulaire de la fiche produit
+
+        // pour commencer, grâce à l'objet Request on récupère l'équivalent de $_SESSION dans Symfony
         $session = $request->getSession();
 
+        // le panier est un tableau array stocké dans $session
+        // si celui-ci n'existe pas au moment où on veut ajouter un produit, alors on le crée...
         if (!$session->get("panier")) {
             $panier = $session->set("panier", []);
         };
 
+        // ... dans tous les cas, on le sélectionne
         $panier = $session->get("panier");
 
+        // on récupère les données envoyées depuis la fiche produit
         $nom = $request->request->get('nom');
         $date_debut = $request->request->get('date_debut');
         $date_fin = $request->request->get('date_fin');
         $prix = $request->request->get('prix');
 
+        // avec array_push, on rajoute un nouvel élément à la fin de $panier, qui sera lui-même un tableau array composé des données recueuillies
         array_push($panier, [$nom, $date_debut, $date_fin, $prix, $slug]);
+        // prochaine étape : faire en sorte qu'on n'ait plus seulement le nom, le prix et le slug, mais l'objet même de l'exemplaire du produit qu'on souhaite louer
+
+        // après avoir manipulé le panier, on le remet à sa place!
         $session->set("panier", $panier);
 
-        return $this->redirectToRoute("gestion_panier");
+        return $this->redirectToRoute("gestion_panier"); // est-ce nécessaire?...
     }
 
     /**
@@ -126,6 +148,11 @@ class ProduitController extends AbstractController
      */
     public function viderPanier(Request $request)
     {
+        // ----------------------
+        // VIDER PANIER
+        // ----------------------
+
+        // on récupère la session pour voir si elle contient un panier : si c'est le cas, alors il devient un tableau array vide
         $session = $request->getSession();
 
         if ($session->get("panier")) {
@@ -140,7 +167,13 @@ class ProduitController extends AbstractController
      */
     public function viderSession(Request $request)
     {
+        // ----------------------
+        // VIDER SESSION
+        // ----------------------
+
+        // l'équivalent de session_destroy(), mais orienté objet (et Symfony-friendly)
         $request->getSession()->clear();
+        // utile pour mettre en place un panier qui va toujours réapparaître : au moment où on est redirectToRoute, on revient dans gestion_panier, où on va vérifier s'il y a un panier, puisqu'il n'y en a pas on va en créer un, etc...
  
         return $this->redirectToRoute("gestion_panier");
     }
