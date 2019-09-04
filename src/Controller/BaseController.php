97bf9cef9d0550777da2416c24b47fdec1efffd3
Marine <marinepelletier@me.com>
2019-09-04 16:12:02 +0200

    MAIL
    MAIL UTD

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -11,23 +11,68 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BaseController extends AbstractController
 {
-    /**
+
+ /**
      * @Route("/contact", name="contact")
      */
-    public function Formulaire(Request $request, ContactNotification $notification)
-    {
-        $contact = new Contact();
-        $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
-            $notification -> notify($contact);
-            $this->addFlash('success', 'Votre mail a bien été envoyé !');
-            $this->redirectToRoute('accueil'); 
+public function Formulaire(Request $request, \Swift_Mailer $mailer){
+
+    $form = $this->createForm(ContactType::class, null);
+    $form -> handleRequest($request);
+
+        // traitement des infos du formulaire
+
+        if($form -> isSubmitted() && $form -> isValid()){
+
+            $data = $form -> getData();
+            // permet de récupérer toutes les infos du formulaire
+            // prenom = $data['prenom']
+            // objet = $data['objet']
+
+            if($this -> sendEmail($data, $mailer)){
+                // $mailer : objet swiftmailer
+                $this -> addFlash('success', 'Votre email a été envoyé et sera traité dans les meilleurs délais.');
+                return $this->redirectToRoute("contact");
+            }
+            else{
+                $this -> addFlash('errors', 'Un problème a eu lieu durant l\'envoie, veuillez ré-essayer plus tard');
+            }
         }
 
-        return $this->render('base/contact.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
+        // Affichage de la vue
+
+    return $this -> render('base/contact.html.twig', [
+      "form" => $form->createView()
+    ]);
+
+    //formulaire de contact une fois envoyé, un message de confirmation apparaît dans la vue
+}
+
+/**
+* Permet d'envoyer des emails
+*
+*/
+public function sendEmail($data, \Swift_Mailer $mailer){
+  $mail = new \Swift_Message();
+  // On instancie un objet swiftmailer en précisant l'objet (sujet) du mail.
+
+  $mail
+    -> setSubject($data['objet'])
+    -> setFrom($data['email'])
+    -> setTo('contact@geekation.com')
+    -> setBody(
+      $this -> renderView('emails/contact.html.twig', [
+        'data' => $data
+      ]), 'text/html'
+    )
+  ;
+
+  if($mailer -> send($mail)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 }
